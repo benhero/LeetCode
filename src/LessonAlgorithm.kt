71656a05712c666b2b7aa19cd909620b7e62695c
benhero <benhero@126.com>
2021-12-02 16:32:01 +0800

    算法： 167. 两数之和 II - 输入有序数组 283. 移动零

--- src/LessonAlgorithm.kt
+++ src/LessonAlgorithm.kt
@@ -1,5 +1,3 @@
-import java.util.*
-
 /**
  *
  * 算法
@@ -10,7 +8,77 @@ object LessonAlgorithm {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        rotate()
+        twoSum()
+    }
+
+    fun twoSum() {
+        // 输入：numbers = [2,7,11,15], target = 9
+        // 输出：[1,2]
+        // 解释：2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+        println(twoSum(intArrayOf(2, 7, 11, 15), 9).contentToString())
+
+        // 输入：numbers = [2,3,4], target = 6
+        // 输出：[1,3]
+        println(twoSum(intArrayOf(2, 3, 4), 6).contentToString())
+
+        // 输入：numbers = [-1,0], target = -1
+        // 输出：[1,2]
+        println(twoSum(intArrayOf(-1, 0), -1).contentToString())
+
+        println(twoSum(intArrayOf(2, 3, 4), 6).contentToString())
+        println(twoSum(intArrayOf(3, 3), 6).contentToString())
+    }
+
+    /**
+     * 167. 两数之和 II - 输入有序数组
+     * 给定一个已按照 非递减顺序排列的整数数组numbers ，请你从数组中找出两个数满足相加之和等于目标数target 。
+     * 函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。numbers 的下标 从 1 开始计数 ，
+     * 所以答案数组应当满足 1 <= answer[0] < answer[1] <= numbers.length 。
+     * 你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+     */
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        var l = 0
+        var r = numbers.size - 1
+        while (l < r) {
+            val temp = numbers[l] + numbers[r]
+            when {
+                temp == target -> {
+                    return intArrayOf(l + 1, r + 1)
+                }
+                temp > target -> {
+                    r--
+                }
+                else -> {
+                    l++
+                }
+            }
+        }
+        return intArrayOf(l + 1, r + 1)
+    }
+
+    fun moveZeroes() {
+        // 输入: [0,1,0,3,12]
+        // 输出: [1,3,12,0,0]
+        moveZeroes(intArrayOf(0, 1, 0, 3, 12))
+        moveZeroes(intArrayOf(1, 3, 12, 0, 0))
+        moveZeroes(intArrayOf(0, 1, 0))
+    }
+
+    /**
+     * 283. 移动零
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     */
+    fun moveZeroes(nums: IntArray): Unit {
+        var zeroCount = 0
+        nums.forEachIndexed { index, value ->
+            if (value == 0) {
+                zeroCount++
+            } else if (zeroCount != 0) {
+                nums[index - zeroCount] = value
+                nums[index] = 0
+            }
+        }
+        println(nums.contentToString())
     }
 
     fun rotate() {
