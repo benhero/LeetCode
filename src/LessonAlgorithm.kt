53025daf5e60c33200f535a3aeeeba88e0adb8b9
benhero <benhero@126.com>
2021-12-10 14:58:04 +0800

    算法： 733. 图像渲染

--- src/LessonAlgorithm.kt
+++ src/LessonAlgorithm.kt
@@ -1,4 +1,5 @@
-import java.lang.StringBuilder
+import java.util.*
+import kotlin.collections.HashMap
 
 /**
  *
@@ -10,7 +11,102 @@ object LessonAlgorithm {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        checkInclusion()
+        floodFill()
+    }
+
+    fun maxAreaOfIsland() {
+
+    }
+
+    /**
+     * 695. 岛屿的最大面积
+     * 给你一个大小为 m x n 的二进制矩阵 grid 。
+     * 岛屿是由一些相邻的1(代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在 水平或者竖直的四个方向上 相邻。你可以假设grid 的四个边缘都被 0（代表水）包围着。
+     * 岛屿的面积是岛上值为 1 的单元格的数目。
+     * 计算并返回 grid 中最大的岛屿面积。如果没有岛屿，则返回面积为 0 。
+     */
+//    fun maxAreaOfIsland(grid: Array<IntArray>): Int {
+//
+//    }
+
+    fun floodFill() {
+        // 输入:
+        // image = [[1,1,1],[1,1,0],[1,0,1]]
+        // sr = 1, sc = 1, newColor = 2
+        // 输出: [[2,2,2],[2,2,0],[2,0,1]]
+        // 解析:
+        // 在图像的正中间，(坐标(sr,sc)=(1,1)),
+        // 在路径上所有符合条件的像素点的颜色都被更改成2。
+        // 注意，右下角的像素没有更改为2，
+        // 因为它不是在上下左右四个方向上与初始点相连的像素点。
+        floodFill(
+            arrayOf(
+                intArrayOf(1, 1, 1),
+                intArrayOf(1, 1, 0),
+                intArrayOf(1, 0, 1)
+            ), 1, 1, 2
+        ).forEach {
+//            it.contentToString().log()
+        }
+
+        floodFill(
+            arrayOf(
+                intArrayOf(0, 0, 0),
+                intArrayOf(0, 1, 1)
+            ), 1, 1, 1
+        ).forEach {
+            it.contentToString().log()
+        }
+
+        floodFill(
+            arrayOf(
+                intArrayOf(0, 0, 1),
+                intArrayOf(0, 0, 0)
+            ), 1, 1, 2
+        ).forEach {
+            it.contentToString().log()
+        }
+    }
+
+    /**
+     * 733. 图像渲染
+     * 有一幅以二维整数数组表示的图画，每一个整数表示该图画的像素值大小，数值在 0 到 65535 之间。=》FFFF，即16位
+     * 给你一个坐标(sr, sc)表示图像渲染开始的像素值（行 ，列）和一个新的颜色值newColor，让你重新上色这幅图像。
+     * 为了完成上色工作，从初始坐标开始，记录初始坐标的上下左右四个方向上像素值与初始坐标相同的相连像素点，
+     * 接着再记录这四个方向上符合条件的像素点与他们对应四个方向上像素值与初始坐标相同的相连像素点，……，重复该过程。
+     * 将所有有记录的像素点的颜色值改为新的颜色值。最后返回经过上色渲染后的图像。
+     */
+    fun floodFill(image: Array<IntArray>, sr: Int, sc: Int, newColor: Int): Array<IntArray> {
+        val oldColor = image[sr][sc]
+        if (oldColor == newColor) {
+            return image
+        }
+        floodFillDfs(image, sr, sc, oldColor, newColor)
+        return image
+    }
+
+    val dRow = intArrayOf(1, 0, 0, -1)
+    val dColumn = intArrayOf(0, 1, -1, 0)
+    val queue = LinkedList<IntArray>()
+    fun floodFillDfs(image: Array<IntArray>, sr: Int, sc: Int, oldColor: Int, newColor: Int) {
+        if (image[sr][sc] == oldColor) {
+            image[sr][sc] = newColor
+            for (i in 0 until 4) {
+                val newRow = sr + dRow[i]
+                val newColumn = sc + dColumn[i]
+                if (isSafe(image, newRow, newColumn) && image[newRow][newColumn] == oldColor) {
+                    queue.add(intArrayOf(newRow, newColumn))
+                }
+            }
+            while (!queue.isEmpty()) {
+                val pop = queue.removeFirst()
+                floodFillDfs(image, pop[0], pop[1], oldColor, newColor)
+            }
+        }
+    }
+
+    fun isSafe(image: Array<IntArray>, x: Int, y: Int): Boolean {
+        return x >= 0 && x < image.size && y >= 0 && y < image[0].size
     }
 
     fun checkInclusion() {
