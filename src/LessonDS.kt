323f61d4663e15f9f986906e756b0837691b7477
BenHero <benhero@126.com>
2021-12-16 13:16:06 +0800

    数据结构： 701. 二叉搜索树中的插入操作

--- src/LessonDS.kt
+++ src/LessonDS.kt
@@ -14,7 +14,43 @@ object LessonDS {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        maxSubArray()
+        insertIntoBST()
+    }
+
+    fun insertIntoBST() {
+        insertIntoBST(arrayToTreeNode(4, 2, 7, 1, 3), 5)?.log()
+        insertIntoBST(arrayToTreeNode(40, 20, 60, 10, 30, 50, 70), 25)?.log()
+    }
+
+    /**
+     * 701. 二叉搜索树中的插入操作
+     * 给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。
+     * 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+     * 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回 任意有效的结果 。
+     */
+    fun insertIntoBST(root: TreeNode?, `val`: Int): TreeNode? {
+        if (root == null) {
+            return TreeNode(`val`)
+        }
+        var node = root
+        while (node != null) {
+            if (`val` < node.`val`) {
+                if (node.left == null) {
+                    node.left = TreeNode(`val`)
+                    return root
+                } else {
+                    node = node.left
+                }
+            } else {
+                if (node.right == null) {
+                    node.right = TreeNode(`val`)
+                    return root
+                } else {
+                    node = node.right
+                }
+            }
+        }
+        return root
     }
 
     fun searchBST() {
@@ -328,6 +364,10 @@ object LessonDS {
     class TreeNode(var `val`: Int) {
         var left: TreeNode? = null
         var right: TreeNode? = null
+
+        override fun toString(): String {
+            return levelOrder(this).toString()
+        }
     }
 
     fun arrayToTreeNode(vararg array: Int?): TreeNode? {
