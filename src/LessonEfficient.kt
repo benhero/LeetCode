e1c3f689f7f65cfe156a74907db022b73f59158a
BenHero <benhero@126.com>
2021-12-21 19:35:10 +0800

    高效制胜: 230. 二叉搜索树中第K小的元素

--- src/LessonEfficient.kt
+++ src/LessonEfficient.kt
@@ -9,7 +9,38 @@ import java.util.*
 object LessonEfficient {
     @JvmStatic
     fun main(args: Array<String>) {
-        nextGreaterElement()
+        kthSmallest()
+    }
+
+    fun kthSmallest() {
+        kthSmallest(arrayToTreeNode(3, 1, 4, null, 2), 1).log()
+        kthSmallest(arrayToTreeNode(5, 3, 6, 2, 4, null, null, 1), 3).log()
+    }
+
+    /**
+     * 230. 二叉搜索树中第K小的元素
+     * 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+     */
+    fun kthSmallest(root: TreeNode?, k: Int): Int {
+        val stack = LinkedList<TreeNode>()
+        var count = 0
+        var node = root
+        while (!stack.isEmpty() || node != null) {
+            // 先把左边迭代到底
+            while (node != null) {
+                stack.push(node)
+                node = node.left
+            }
+            node = stack.pop()
+//            node.`val`.log()
+            count++
+            if (count == k) {
+                return node.`val`
+            }
+
+            node = node.right
+        }
+        return -1
     }
 
     fun nextGreaterElement() {
@@ -267,5 +298,114 @@ object LessonEfficient {
             }
         }
         return result
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+
+        override fun toString(): String {
+            return levelOrder(this).toString()
+        }
+    }
+
+    fun arrayToTreeNode(vararg array: Int?): TreeNode? {
+        if (array.isEmpty()) {
+            return null
+        }
+        val linkedList = LinkedList<TreeNode>()
+        val root = TreeNode(array[0]!!)
+        if (array.size == 1) {
+            return root
+        }
+        linkedList.add(root)
+
+        var i = 1
+        while (i < array.size) {
+            var node = array[i]?.let { TreeNode(it) }
+            val removeFirst = linkedList.removeFirst()
+
+            removeFirst.left = node
+            node?.let { linkedList.add(it) }
+
+            i++
+            if (i == array.size) {
+                break
+            }
+
+            node = array[i]?.let { TreeNode(it) }
+            removeFirst.right = node
+            i++
+            node?.let { linkedList.add(it) }
+        }
+
+        return root
+    }
+
+    /**
+     * 102. 二叉树的层序遍历
+     * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+     */
+    fun levelOrder(root: TreeNode?): List<List<Int>> {
+        // 临时队列，用于缓存待遍历的节点
+        val queue = LinkedList<TreeNode>()
+        val result = arrayListOf<ArrayList<Int>>()
+        if (root == null) {
+            return result
+        }
+        queue.add(root)
+        while (queue.isNotEmpty()) {
+            val list = arrayListOf<Int>()
+            val size = queue.size
+
+            for (i in 0 until size) {
+                // 移除头部节点
+                val node = queue.removeFirst()
+                list.add(node.`val`)
+                node.left?.let {
+                    // 找到下一层的节点，并加入到待遍历的队列
+                    queue.add(it)
+
+                }
+                node.right?.let {
+                    queue.add(it)
+                }
+            }
+            result.add(list)
+        }
+        return result
+
+
+//        val queue = ArrayList<TreeNode>()
+//        val result = arrayListOf<ArrayList<Int>>()
+//        root?.let {
+//            queue.add(it)
+//            result.add(arrayListOf(root.`val`))
+//        }
+//        while (queue.isNotEmpty()) {
+//            val list = arrayListOf<Int>()
+//            val size = queue.size
+//
+//            for (i in 0 until size) {
+//                val node = queue[i]
+//                node.left?.let {
+//                    // 找到下一层的节点，并加入到待遍历的队列，同时添加到结果队列中
+//                    queue.add(it)
+//                    list.add(it.`val`)
+//                }
+//                node.right?.let {
+//                    queue.add(it)
+//                    list.add(it.`val`)
+//                }
+//            }
+//            // 移除已经遍历完成的节点
+//            for (i in 0 until size) {
+//                queue.removeAt(0)
+//            }
+//            if (list.isNotEmpty()) {
+//                result.add(list)
+//            }
+//        }
+//        return result
     }
 }
